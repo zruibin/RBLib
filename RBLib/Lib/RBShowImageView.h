cff17adbb90ad1155b6b1427ec35de6b98bbd6fc
zruibin <zruibin@PangdeMac-mini-2.local>
2015-05-04 15:14:26 +0800

    添加RBShowImageView

---
+++ RBLib/Lib/RBShowImageView.h
@@ -0,0 +1,29 @@
+//
+//  RBShowImageView.h
+//  RBLib
+//
+//  Created by zruibin on 15/4/14.
+//  Copyright (c) 2015年 zruibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RBShowImageView : UIView<UIScrollViewDelegate>
+{
+    UIImageView *showImage;
+}
+
+
+/**
+ *  初始化
+ *
+ *  @param imgs 要展示的图片数组(数组内容为UIImage)
+ *
+ *  @return 该对象
+ */
+- (instancetype)initWithImages:(NSArray *)imgs;
+
+- (void)show;
+
+@end
