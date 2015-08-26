3745dcd7df46187020e450f35a7bc9a71e60eac7
zhouruibin <zhouruibin@Litter-bear.local>
2015-08-26 17:42:42 +0800

    添加使用blend改变图片颜色UIImage+Tint

---
+++ RBLib/Lib/ImageBlend/UIImage+Tint.h
@@ -0,0 +1,21 @@
+//
+//  UIImage+Tint.h
+//  ImageBlend
+//
+//  Created by 王 巍 on 13-4-29.
+//  Copyright (c) 2013年 OneV-s-Den. All rights reserved.
+//
+
+/*
+ * http://onevcat.com/2013/04/using-blending-in-ios/
+ * https://github.com/onevcat/VVImageTint
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Tint)
+
+- (UIImage *) imageWithTintColor:(UIColor *)tintColor;
+- (UIImage *) imageWithGradientTintColor:(UIColor *)tintColor;
+
+@end
