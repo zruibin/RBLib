5edeb73620b7e44da45f7002a6d960daa3d1673b
zruibin <ruibin.chow@qq.com>
2016-06-14 12:32:41 +0800

    序列化与反序列集成方案

--- RBLib/Lib/RBMacros.h
+++ RBLib/Lib/RBMacros.h
@@ -179,6 +179,175 @@ BLOCK_EXEC(completionBlock, arg1, arg2);
 #define RB_TEST_NOTIFY  \
     [[NSNotificationCenter defaultCenter]postNotificationName:@"RBBaseTest" object:nil];
 
+
+#pragma mark --- 序列化与反序列集成方案
+
+/*
+ *  (CN)本文件提供iOS下的序列化与反序列集成方案，几行代码即可轻松实现。
+ *  在传统方式中当你需要对某个对象进行序列化与反序列化时，往往需要实现<NSCoding>协议以及<NSCopying>协议(非必须)，
+ *  在协议方法中对需要序列化的变量执行encode&decode操作。当变量较多并且需要序列化的自定义对象较多时容易出现过多冗余代码，稍有不慎容易出错。
+ *  RBSerializeKit在运行时对类以及所有层级父类的变量进行遍历，避免遗漏。属性越多，优势越明显。
+ */
+
+#define RBSERIALIZE_CODER_DECODER()     \
+\
+- (id)initWithCoder:(NSCoder *)coder    \
+{   \
+    DLog(@"%s",__func__);  \
+    Class cls = [self class];   \
+    while (cls != [NSObject class]) {   \
+        /*判断是自身类还是父类*/    \
+        BOOL bIsSelfClass = (cls == [self class]);  \
+        unsigned int iVarCount = 0; \
+        unsigned int propVarCount = 0;  \
+        unsigned int sharedVarCount = 0;    \
+        Ivar *ivarList = bIsSelfClass ? class_copyIvarList([cls class], &iVarCount) : NULL;/*变量列表，含属性以及私有变量*/   \
+        objc_property_t *propList = bIsSelfClass ? NULL : class_copyPropertyList(cls, &propVarCount);/*属性列表*/   \
+        sharedVarCount = bIsSelfClass ? iVarCount : propVarCount;   \
+        \
+        for (int i = 0; i < sharedVarCount; i++) {  \
+            const char *varName = bIsSelfClass ? ivar_getName(*(ivarList + i)) : property_getName(*(propList + i)); \
+            NSString *key = [NSString stringWithUTF8String:varName];   \
+            id varValue = [coder decodeObjectForKey:key];   \
+            if (varValue) { \
+                [self setValue:varValue forKey:key];    \
+            }   \
+        }   \
+        free(ivarList); \
+        free(propList); \
+        cls = class_getSuperclass(cls); \
+    }   \
+    return self;    \
+}   \
+\
+- (void)encodeWithCoder:(NSCoder *)coder    \
+{   \
+    DLog(@"%s",__func__);  \
+    Class cls = [self class];   \
+    while (cls != [NSObject class]) {   \
+        /*判断是自身类还是父类*/    \
+        BOOL bIsSelfClass = (cls == [self class]);  \
+        unsigned int iVarCount = 0; \
+        unsigned int propVarCount = 0;  \
+        unsigned int sharedVarCount = 0;    \
+        Ivar *ivarList = bIsSelfClass ? class_copyIvarList([cls class], &iVarCount) : NULL;/*变量列表，含属性以及私有变量*/   \
+        objc_property_t *propList = bIsSelfClass ? NULL : class_copyPropertyList(cls, &propVarCount);/*属性列表*/ \
+        sharedVarCount = bIsSelfClass ? iVarCount : propVarCount;   \
+        \
+        for (int i = 0; i < sharedVarCount; i++) {  \
+            const char *varName = bIsSelfClass ? ivar_getName(*(ivarList + i)) : property_getName(*(propList + i)); \
+            NSString *key = [NSString stringWithUTF8String:varName];    \
+            /*valueForKey只能获取本类所有变量以及所有层级父类的属性，不包含任何父类的私有变量(会崩溃)*/  \
+            id varValue = [self valueForKey:key];   \
+            if (varValue) { \
+                [coder encodeObject:varValue forKey:key];   \
+            }   \
+        }   \
+        free(ivarList); \
+        free(propList); \
+        cls = class_getSuperclass(cls); \
+    }   \
+}
+
+
+#define RBSERIALIZE_COPY_WITH_ZONE()  \
+\
+/*如果不实现copyWithZone:方法，则[personObject copy]时会崩溃*/   \
+- (id)copyWithZone:(NSZone *)zone   \
+{   \
+    DLog(@"%s",__func__);  \
+    id copy = [[[self class] allocWithZone:zone] init];    \
+    Class cls = [self class];   \
+    while (cls != [NSObject class]) {  \
+        /*判断是自身类还是父类*/    \
+        BOOL bIsSelfClass = (cls == [self class]);  \
+        unsigned int iVarCount = 0; \
+        unsigned int propVarCount = 0;  \
+        unsigned int sharedVarCount = 0;    \
+        Ivar *ivarList = bIsSelfClass ? class_copyIvarList([cls class], &iVarCount) : NULL;/*变量列表，含属性以及私有变量*/   \
+        objc_property_t *propList = bIsSelfClass ? NULL : class_copyPropertyList(cls, &propVarCount);/*属性列表*/   \
+        sharedVarCount = bIsSelfClass ? iVarCount : propVarCount;   \
+        \
+        for (int i = 0; i < sharedVarCount; i++) {  \
+            const char *varName = bIsSelfClass ? ivar_getName(*(ivarList + i)) : property_getName(*(propList + i)); \
+            NSString *key = [NSString stringWithUTF8String:varName];    \
+            /*valueForKey只能获取本类所有变量以及所有层级父类的属性，不包含任何父类的私有变量(会崩溃)*/  \
+            id varValue = [self valueForKey:key];   \
+            if (varValue) { \
+                [copy setValue:varValue forKey:key];    \
+            }   \
+        }   \
+        free(ivarList); \
+        free(propList); \
+        cls = class_getSuperclass(cls); \
+    }   \
+    return copy;    \
+}
+
+
+#define RBSERIALIZE_DESCRIPTION() \
+\
+/* 用来打印本类的所有变量(成员变量+属性变量)，所有层级父类的属性变量及其对应的值 */  \
+- (NSString *)description   \
+{   \
+    NSString  *despStr = @"";   \
+    Class cls = [self class];   \
+    while (cls != [NSObject class]) {   \
+        /*判断是自身类还是父类*/  \
+        BOOL bIsSelfClass = (cls == [self class]);  \
+        unsigned int iVarCount = 0; \
+        unsigned int propVarCount = 0;  \
+        unsigned int sharedVarCount = 0;    \
+        Ivar *ivarList = bIsSelfClass ? class_copyIvarList([cls class], &iVarCount) : NULL;/*变量列表，含属性以及私有变量*/   \
+        objc_property_t *propList = bIsSelfClass ? NULL : class_copyPropertyList(cls, &propVarCount);/*属性列表*/   \
+        sharedVarCount = bIsSelfClass ? iVarCount : propVarCount;   \
+        \
+        for (int i = 0; i < sharedVarCount; i++) {  \
+            const char *varName = bIsSelfClass ? ivar_getName(*(ivarList + i)) : property_getName(*(propList + i)); \
+            NSString *key = [NSString stringWithUTF8String:varName];    \
+            /*valueForKey只能获取本类所有变量以及所有层级父类的属性，不包含任何父类的私有变量(会崩溃)*/  \
+            id varValue = [self valueForKey:key];   \
+            if (varValue) { \
+                despStr = [despStr stringByAppendingString:[NSString stringWithFormat:@"%@: %@\n", key, varValue]]; \
+            }   \
+        }   \
+        free(ivarList); \
+        free(propList); \
+        cls = class_getSuperclass(cls); \
+    }   \
+    return despStr; \
+}
+
+
+/* 封装归档keyedArchiver操作 */
+#define RBSERIALIZE_ARCHIVE(__objToBeArchived__, __key__, __filePath__)    \
+\
+NSMutableData *data = [NSMutableData data]; \
+NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:data];   \
+[archiver encodeObject:__objToBeArchived__ forKey:__key__];    \
+[archiver finishEncoding];  \
+[data writeToFile:__filePath__ atomically:YES]
+
+
+/* 封装反归档keyedUnarchiver操作 */
+#define RBSERIALIZE_UNARCHIVE(__objToStoreData__, __key__, __filePath__)   \
+NSMutableData *dedata = [NSMutableData dataWithContentsOfFile:__filePath__]; \
+NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:dedata];  \
+__objToStoreData__ = [unarchiver decodeObjectForKey:__key__];  \
+[unarchiver finishDecoding]
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif
 
 
