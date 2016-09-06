70019c5ca56f3e291f9f4f595af1eecabfa3a134
zruibin <ruibin.chow@qq.com>
2016-09-06 15:17:41 +0800

    add RBGCDTimer

---
+++ RBLib/Lib/RBGCDTimer/RBGCDTimer.m
@@ -0,0 +1,161 @@
+//
+//  RBGCDTimer.m
+//  RBLib
+//
+//  Created by Ruibin.Chow on 16/9/6.
+//  Copyright © 2016年 Ruibin.Chow. All rights reserved.
+//
+
+#import "RBGCDTimer.h"
+
+@interface RBGCDTimer ()
+
+@property (nonatomic, strong) NSMutableDictionary *timerContainer;
+@property (nonatomic, strong) NSMutableDictionary *actionBlockCache;
+
+@end
+
+@implementation RBGCDTimer
+
+#pragma mark - Public Method
+
++ (RBGCDTimer *)sharedInstance
+{
+    static RBGCDTimer *gcdTimer = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken,^{
+        gcdTimer = [[RBGCDTimer alloc] init];
+    });
+    
+    return gcdTimer;
+}
+
+- (void)scheduledDispatchTimerWithName:(NSString *)timerName
+                          timeInterval:(double)interval
+                                 queue:(dispatch_queue_t)queue
+                               repeats:(BOOL)repeats
+                          actionOption:(RBActionOption)option
+                                action:(dispatch_block_t)action
+{
+    if (nil == timerName)
+        return;
+    
+    if (nil == queue)
+        queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    dispatch_source_t timer = [self.timerContainer objectForKey:timerName];
+    if (!timer) {
+        timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+        dispatch_resume(timer);
+        [self.timerContainer setObject:timer forKey:timerName];
+    }
+    
+    /* timer精度为0.1秒 */
+    dispatch_source_set_timer(timer, dispatch_time(DISPATCH_TIME_NOW, interval * NSEC_PER_SEC), interval * NSEC_PER_SEC, 0.1 * NSEC_PER_SEC);
+    
+    __weak typeof(self) weakSelf = self;
+    
+    switch (option) {
+            
+        case RBAbandonPreviousAction:
+        {
+            /* 移除之前的action */
+            [weakSelf removeActionCacheForTimer:timerName];
+            
+            dispatch_source_set_event_handler(timer, ^{
+                action();
+                
+                if (!repeats) {
+                    [weakSelf cancelTimerWithName:timerName];
+                }
+            });
+        }
+            break;
+            
+        case RBMergePreviousAction:
+        {
+            /* cache本次的action */
+            [self cacheAction:action forTimer:timerName];
+            
+            dispatch_source_set_event_handler(timer, ^{
+                NSMutableArray *actionArray = [self.actionBlockCache objectForKey:timerName];
+                [actionArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                    dispatch_block_t actionBlock = obj;
+                    actionBlock();
+                }];
+                [weakSelf removeActionCacheForTimer:timerName];
+                
+                if (!repeats) {
+                    [weakSelf cancelTimerWithName:timerName];
+                }
+            });
+        }
+            break;
+    }
+}
+
+- (void)cancelTimerWithName:(NSString *)timerName
+{
+    dispatch_source_t timer = [self.timerContainer objectForKey:timerName];
+    
+    if (!timer) {
+        return;
+    }
+    
+    [self.timerContainer removeObjectForKey:timerName];
+    dispatch_source_cancel(timer);
+    
+    [self.actionBlockCache removeObjectForKey:timerName];
+}
+
+- (void)cancelAllTimer
+{
+    // Fast Enumeration
+    [self.timerContainer enumerateKeysAndObjectsUsingBlock:^(NSString *timerName, dispatch_source_t timer, BOOL *stop) {
+        [self.timerContainer removeObjectForKey:timerName];
+        dispatch_source_cancel(timer);
+    }];
+}
+
+#pragma mark - Property
+
+- (NSMutableDictionary *)timerContainer
+{
+    if (!_timerContainer) {
+        _timerContainer = [[NSMutableDictionary alloc] init];
+    }
+    return _timerContainer;
+}
+
+- (NSMutableDictionary *)actionBlockCache
+{
+    if (!_actionBlockCache) {
+        _actionBlockCache = [[NSMutableDictionary alloc] init];
+    }
+    return _actionBlockCache;
+}
+
+#pragma mark - Private Method
+
+- (void)cacheAction:(dispatch_block_t)action forTimer:(NSString *)timerName
+{
+    id actionArray = [self.actionBlockCache objectForKey:timerName];
+    
+    if (actionArray && [actionArray isKindOfClass:[NSMutableArray class]]) {
+        [(NSMutableArray *)actionArray addObject:action];
+    }else {
+        NSMutableArray *array = [NSMutableArray arrayWithObject:action];
+        [self.actionBlockCache setObject:array forKey:timerName];
+    }
+}
+
+- (void)removeActionCacheForTimer:(NSString *)timerName
+{
+    if (![self.actionBlockCache objectForKey:timerName])
+        return;
+    
+    [self.actionBlockCache removeObjectForKey:timerName];
+}
+
+@end
